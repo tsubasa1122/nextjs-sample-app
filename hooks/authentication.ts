0b043924b43ffadc93a99a9f7c628b41d10a6ef9
tsubasa1122 <nakanot@open8.com>
2020-10-31 23:31:08 +0900

    Add user show

--- hooks/authentication.ts
+++ hooks/authentication.ts
@@ -41,6 +41,7 @@ export function useAuthentication() {
         const loginUser: User = {
           uid: firebaseUser.uid,
           isAnonymous: firebaseUser.isAnonymous,
+          name: '',
         };
 
         setUser(loginUser);
