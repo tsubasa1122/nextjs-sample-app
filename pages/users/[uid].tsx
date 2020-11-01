5d77ad7d80781907d0144083559a007368a00654
tsubasa1122 <nakanot@open8.com>
2020-11-01 19:17:56 +0900

    Add question form

--- pages/users/[uid].tsx
+++ pages/users/[uid].tsx
@@ -10,6 +10,8 @@ type Query = {
 
 export default function UserShow() {
   const [user, setUser] = useState<User>(null);
+  const [body, setBody] = useState('');
+  const [isSending, setIsSending] = useState(false);
   const router = useRouter();
   const query = router.query as Query;
 
@@ -32,12 +34,47 @@ export default function UserShow() {
     }
     loadUser();
   }, [query.uid]);
+
+  async function onSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    await firebase.firestore().collection('questions').add({
+      senderUid: firebase.auth().currentUser.uid,
+      receiverUid: user.uid,
+      body,
+      isReplied: false,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+
+    setBody('');
+    alert('質問を送信しました');
+  }
+
   return (
     <Layout>
       {user && (
         <div className="text-center">
           <h1 className="h4">{user.name}さんのページ</h1>
           <div className="m-5">{user.name}さんに質問しよう</div>
+          <div className="row justify-content-center mb-3">
+            <div className="col-12 col-md-6">
+              <form onSubmit={onSubmit}>
+                <textarea
+                  className="form-control"
+                  placeholder="おげんきですか？"
+                  rows={6}
+                  value={body}
+                  onChange={(e) => setBody(e.target.value)}
+                  required
+                ></textarea>
+                <div className="m-3">
+                  <button type="submit" className="btn btn-primary">
+                    質問を送信する
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
       )}
     </Layout>
