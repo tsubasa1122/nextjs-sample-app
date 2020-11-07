152da9b62f290235c044f04f4ed1d24f223cda45
tsubasa1122 <nakanot@open8.com>
2020-11-07 20:50:13 +0900

    Add question detail

--- pages/questions/received.tsx
+++ pages/questions/received.tsx
@@ -1,6 +1,7 @@
 import * as firebase from 'firebase/app';
 import { useState, useEffect, useRef } from 'react';
 import dayjs from 'dayjs';
+import Link from 'next/link';
 import { useAuthentication } from '../../hooks/authentication';
 import { Question } from '../../models/Question';
 import Layout from '../../components/Layout';
@@ -104,18 +105,24 @@ export default function QuestionReceived() {
       <div className="row justify-content-center">
         <div className="col-12 col-md-6" ref={scrollContainerRef}>
           {questions.map((question) => (
-            <div className="card my-3" key={question.id}>
-              <div className="card-body">
-                <div className="text-truncate">{question.body}</div>
-                <div className="text-muted text-right">
-                  <small>
-                    {dayjs(question.createdAt.toDate()).format(
-                      'YYYY/MM/DD HH:mm'
-                    )}
-                  </small>
+            <Link
+              href="/questions/[id]"
+              as={`/questions/${question.id}`}
+              key={question.id}
+            >
+              <div className="card my-3" key={question.id}>
+                <div className="card-body">
+                  <div className="text-truncate">{question.body}</div>
+                  <div className="text-muted text-right">
+                    <small>
+                      {dayjs(question.createdAt.toDate()).format(
+                        'YYYY/MM/DD HH:mm'
+                      )}
+                    </small>
+                  </div>
                 </div>
               </div>
-            </div>
+            </Link>
           ))}
         </div>
       </div>
