f851774c1fb9a79c8f54e01f8eb268f20c3b3609
tsubasa1122 <nakanot@open8.com>
2020-11-28 20:26:31 +0900

    Add answer content to canvas

--- pages/api/answers/[id]/ogp.ts
+++ pages/api/answers/[id]/ogp.ts
@@ -1,8 +1,27 @@
 import * as path from 'path';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { createCanvas, registerFont } from 'canvas';
+import '../../../../lib/firebase_admin';
+import { firestore } from 'firebase-admin';
+import { Answer } from '../../../../models/Answer';
+import { Question } from '../../../../models/Question';
+
+type SeparatedText = {
+  line: string;
+  remaining: string;
+};
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const id = req.query.id as string;
+
+  const answerDoc = await firestore().collection('answers').doc(id).get();
+  const answer = answerDoc.data() as Answer;
+  const questionDoc = await firestore()
+    .collection('questions')
+    .doc(answer.questionId)
+    .get();
+  const question = questionDoc.data() as Question;
+
   registerFont(path.resolve('./fonts/ipagp.ttf'), {
     family: 'ipagp',
   });
@@ -18,7 +37,11 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   context.fillStyle = '#424242';
   context.textAlign = 'center';
   context.textBaseline = 'middle';
-  context.fillText('testテスト', 100, 50);
+  const lines = createTextLines(context, question.body);
+  lines.forEach((line, index) => {
+    const y = 157 + 40 * (index - (lines.length - 1) / 2);
+    context.fillText(line, 300, y);
+  });
 
   const buffer = canvas.toBuffer();
 
@@ -28,3 +51,35 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   });
   res.end(buffer, 'binary');
 };
+
+function createTextLine(context, text: string): SeparatedText {
+  const maxWidth = 400;
+
+  for (let i = 0; i < text.length; i++) {
+    const line = text.substring(0, i + 1);
+    if (context.measureText(line).width > maxWidth) {
+      return {
+        line,
+        remaining: text.substring(i + 1),
+      };
+    }
+  }
+
+  return {
+    line: text,
+    remaining: '',
+  };
+}
+
+function createTextLines(context, text: string): string[] {
+  const lines: string[] = [];
+  let currentText = text;
+
+  while (currentText !== '') {
+    const separatedText = createTextLine(context, currentText);
+    lines.push(separatedText.line);
+    currentText = separatedText.remaining;
+  }
+
+  return lines;
+}
