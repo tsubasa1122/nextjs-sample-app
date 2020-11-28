2eb258cb5bb8454b445a8ba6dc033e3a1dc93bef
tsubasa1122 <nakanot@open8.com>
2020-11-28 20:08:17 +0900

    Add font test

--- pages/api/answers/[id]/ogp.ts
+++ pages/api/answers/[id]/ogp.ts
@@ -1,14 +1,24 @@
+import * as path from 'path';
 import { NextApiRequest, NextApiResponse } from 'next';
-import { createCanvas } from 'canvas';
+import { createCanvas, registerFont } from 'canvas';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+  registerFont(path.resolve('./fonts/ipagp.ttf'), {
+    family: 'ipagp',
+  });
   const width = 600;
   const height = 315;
   const canvas = createCanvas(width, height);
   const context = canvas.getContext('2d');
 
-  context.fillStyle = '#888888';
+  context.fillStyle = '#fafafa';
   context.fillRect(0, 0, width, height);
+
+  context.font = '20px ipagp';
+  context.fillStyle = '#424242';
+  context.textAlign = 'center';
+  context.textBaseline = 'middle';
+  context.fillText('testテスト', 100, 50);
 
   const buffer = canvas.toBuffer();
 
